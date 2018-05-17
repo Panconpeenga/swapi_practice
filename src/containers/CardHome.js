83322bc64cde983e3dc40009ea92c8bd8331396d
Panconpeenga <imsoto@icloud.com>
2018-05-17 03:37:18 -0400

    added the deathstar for testing

--- src/containers/CardHome.js
+++ src/containers/CardHome.js
@@ -6,6 +6,10 @@ class CardHome extends Component {
   return (
     <div className=''>
       <p className='f1'>Welcome! Click on a category above to get started!</p>
+      <img
+        className='h4 w4'
+      src='/img/Death_Star_icon.png'
+      alt='death star'/>
     </div>
   )
   }
