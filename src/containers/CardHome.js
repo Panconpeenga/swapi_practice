5f9ac5a051066db9c0defe65b567745f43cede32
Panconpeenga <imsoto@icloud.com>
2020-04-08 17:33:31 -0400

    well... there was an api to reference

--- src/containers/CardHome.js
+++ src/containers/CardHome.js
@@ -1,17 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import deathstar from './Death_Star_icon.png';
 
 class CardHome extends Component {
   render() {
-
-  return (
-    <div className=''>
-      <p className='f1'>Welcome! Click on a category above to get started!</p>
-      <img
-        className='h4 w4'
-      src='/img/Death_Star_icon.png'
-      alt='death star'/>
-    </div>
-  )
+    return (
+      <div className=''>
+        <p className='f1'>Welcome! Click on a category above to get started!</p>
+        <img className='h4 w4' src={deathstar} alt='death star' />
+      </div>
+    );
   }
 }
 
